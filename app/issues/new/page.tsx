c3f5bb16b15e0e7c592438af38688729c2b1cb33
Iftekhar Hyder <iftekharx86@gmail.com>
2023-10-13 20:04:59 +0600

    test from l

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+const NewIssuePage = () => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (err) {
+            setError("Unknown error occured! ");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme appearance="light" accentColor="purple">
+          <NavBar />
+          <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   );
